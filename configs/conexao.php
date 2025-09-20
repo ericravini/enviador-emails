6b0c4859fe7cbe2866b6b69ed104c101a9900dbc
Eric Ravini <ericvilela1512@gmail.com>
2025-09-20 18:48:35 +0000

    🖥 Melhoria na Interface

--- configs/conexao.php
+++ configs/conexao.php
@@ -10,14 +10,15 @@
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Criando table caso não exista.
-    $table_cmd = "CREATE TABLE IF NOT EXISTS usuarios (
-      id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
-      nome VARCHAR(40), 
-      email VARCHAR(320) 
-    )";
+    $table_cmd = "CREATE TABLE IF NOT EXISTS contatos (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      nome VARCHAR(100) NOT NULL,
+      email VARCHAR(150) NOT NULL,
+      criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );";
     $query = $pdo->prepare($table_cmd);
     $query->execute();
 
   } catch (PDOException $e) {
-    echo "Erro na conexão da database: " . $e->getMessage(); //o getmessage aqui pega a mensagem do erro, obviamente
+    echo '<p style="color=red;">Erro na conexão da database: ' . $e->getMessage() . '</p>'; //o getmessage aqui pega a mensagem do erro, obviamente
   }
